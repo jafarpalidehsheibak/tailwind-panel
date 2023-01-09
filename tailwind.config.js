7a11b2d0aafb2a42e7fb252726a009b6c4d7049e
jafarpalidehsheibak <jafar.palideh.sheibak@gmail.com>
2023-01-09 08:28:38 +0330

    add font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.*"],
+  theme: {
+    fontFamily: {
+      body: ["vazir"],
+      display: ["vazir"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
